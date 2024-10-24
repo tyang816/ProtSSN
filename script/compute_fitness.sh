ace6a0bd3aaca9ac2f856839a777f9527b5f4db0
tyang816 <ty_ang@163.com>
2024-10-24 22:46:10 +0800

    refactor: Rename zeroshot_predict.sh to compute_fitness.sh and update script
    - Renamed the file zeroshot_predict.sh to compute_fitness.sh to better reflect its purpose.
    - Updated the script to use the new file name in the command.
    - Also made some minor changes to the comments and variable names for clarity.

--- script/compute_fitness.sh
+++ script/compute_fitness.sh
@@ -2,7 +2,7 @@
 # h in (512, 768, 1280)
 # use single model for inference (default)
 DATASET=proteingym-benchmark
-CUDA_VISIBLE_DEVICES=0 python zeroshot_predict.py \
+CUDA_VISIBLE_DEVICES=0 python compute_fitness.py \
     --gnn_model_name k10_h512 \
     --mutant_dataset_dir data/mutant_example/$DATASET \
     --result_dir result/$DATASET
@@ -10,7 +10,7 @@ CUDA_VISIBLE_DEVICES=0 python zeroshot_predict.py \
 
 # select the models for ensemble prediction
 DATASET=proteingym-benchmark
-CUDA_VISIBLE_DEVICES=0 python zeroshot_predict.py \
+CUDA_VISIBLE_DEVICES=0 python compute_fitness.py \
     --gnn_model_name k10_h512 k20_h512 k30_h512 k10_h768 k20_h768 k30_h768 k10_h1280 k20_h1280 k30_h1280 \
     --use_ensemble \
     --mutant_dataset_dir data/mutant_example/$DATASET \
